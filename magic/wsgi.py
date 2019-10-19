ea255ef60c6892d0d74561bc7e167c8c1675c3a6
thetestgame <nxtlabs13@gmail.com>
2019-10-19 12:13:36 -0500

    magic/wsgi: Fixed incorrect import

--- magic/wsgi.py
+++ magic/wsgi.py
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from service import create_app
+from magic import create_app
 import os
 
 # Create app
